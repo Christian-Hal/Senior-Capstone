95d4d7e718f27c8fea3770b3ce1700d6b462800c
Gavin Dominique <gavindom315@gmail.com>
2025-12-14 16:37:03 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
-// ALL MAJOR CODE SHOULD GO HERE
-// THIS IS THE FILE BUILT AND RUN
+// ALL MAJOR CODE SHOULD GO HERE. THIS IS THE FILE BUILT AND RUN.
+// To build and run:
+// In terminal cd into the repo and then cd into the build folder
+// run the command "cmake --build ."
+// then run "./Capstone" on mac or "Capstone.exe" on windows
 
 #include <iostream>
 
