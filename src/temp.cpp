2e4c255ced4725c21564f866e360e4afbc729791
CDees <bcd021@latech.edu>
2025-12-17 13:52:55 -0600

    Change OpenGL version

---
+++ src/temp.cpp
@@ -0,0 +1,15 @@
+// ALL MAJOR CODE SHOULD GO HERE. THIS IS THE FILE BUILT AND RUN.
+// To build and run:
+// In terminal cd into the repo and then cd into the build folder
+// run the command "cmake --build ."
+// then run "./Capstone" on mac or "Capstone.exe" on windows
+
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
